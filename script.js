2b841e4e79a4658c4c0a3482aad90bfef61ca7f9
René Hoffmann <hoffelmann@gmail.com>
2024-10-10 20:47:43 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function safeParseInt(str) {
+    return str === "" ? 0 : parseInt(str, 10);
+}
+
+function calculate() {
+    _game1_upper_sum =
+        safeParseInt(game1_upper_1.value) +
+        safeParseInt(game1_upper_2.value) +
+        safeParseInt(game1_upper_3.value) +
+        safeParseInt(game1_upper_4.value) +
+        safeParseInt(game1_upper_5.value) +
+        safeParseInt(game1_upper_6.value)
+
+    if (safeParseInt(game1_upper_1.value) +
+        safeParseInt(game1_upper_2.value) +
+        safeParseInt(game1_upper_3.value) +
+        safeParseInt(game1_upper_4.value) +
+        safeParseInt(game1_upper_5.value) +
+        safeParseInt(game1_upper_6.value) > 62) {
+        _game1_upper_bonus = 35
+    } else {
+        _game1_upper_bonus = 0
+    }
+
+    _game1_lower_sum =
+        safeParseInt(game1_lower_3.value) +
+        safeParseInt(game1_lower_4.value) +
+        (game1_lower_fullHouse.checked === true ? 25 : 0) +
+        (game1_lower_smallStreet.checked === true ? 30 : 0) +
+        (game1_lower_bigStreet.checked === true ? 40 : 0) +
+        (game1_lower_kniffel.checked === true ? 50 : 0) +
+        safeParseInt(game1_lower_chance.value)
+
+    _game1_total_sum = _game1_upper_sum + _game1_upper_bonus + _game1_lower_sum
+
+    game1_upper_sum.innerText = _game1_upper_sum
+    game1_upper_bonus.innerText = _game1_upper_bonus
+    game1_lower_sum.innerText = _game1_lower_sum
+    game1_total_sum.innerText = _game1_total_sum
+}
