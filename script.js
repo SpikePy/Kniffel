8419f237ff06202a1ba181f877a8cf7d5181271c
René Hoffmann <hoffelmann@gmail.com>
2024-10-14 11:25:03 +0200

    calculate all every time something changes

--- script.js
+++ script.js
@@ -41,4 +41,144 @@ function calculate() {
     game1_upper_bonus.innerText = _game1_upper_bonus
     game1_lower_sum.innerText = _game1_lower_sum
     game1_total_sum.innerText = _game1_total_sum
+
+    _game2_upper_sum =
+        safeParseInt(game2_upper_1.value) +
+        safeParseInt(game2_upper_2.value) +
+        safeParseInt(game2_upper_3.value) +
+        safeParseInt(game2_upper_4.value) +
+        safeParseInt(game2_upper_5.value) +
+        safeParseInt(game2_upper_6.value)
+
+    if (safeParseInt(game2_upper_1.value) +
+        safeParseInt(game2_upper_2.value) +
+        safeParseInt(game2_upper_3.value) +
+        safeParseInt(game2_upper_4.value) +
+        safeParseInt(game2_upper_5.value) +
+        safeParseInt(game2_upper_6.value) > 62) {
+        _game2_upper_bonus = 35
+    } else {
+        _game2_upper_bonus = 0
+    }
+
+    _game2_lower_sum =
+        safeParseInt(game2_lower_3.value) +
+        safeParseInt(game2_lower_4.value) +
+        (game2_lower_fullHouse.checked === true ? 25 : 0) +
+        (game2_lower_smallStreet.checked === true ? 30 : 0) +
+        (game2_lower_bigStreet.checked === true ? 40 : 0) +
+        (game2_lower_kniffel.checked === true ? 50 : 0) +
+        safeParseInt(game2_lower_chance.value)
+
+    _game2_total_sum = _game2_upper_sum + _game2_upper_bonus + _game2_lower_sum
+
+    game2_upper_sum.innerText = _game2_upper_sum
+    game2_upper_bonus.innerText = _game2_upper_bonus
+    game2_lower_sum.innerText = _game2_lower_sum
+    game2_total_sum.innerText = _game2_total_sum
+
+    _game3_upper_sum =
+        safeParseInt(game3_upper_1.value) +
+        safeParseInt(game3_upper_2.value) +
+        safeParseInt(game3_upper_3.value) +
+        safeParseInt(game3_upper_4.value) +
+        safeParseInt(game3_upper_5.value) +
+        safeParseInt(game3_upper_6.value)
+
+    if (safeParseInt(game3_upper_1.value) +
+        safeParseInt(game3_upper_2.value) +
+        safeParseInt(game3_upper_3.value) +
+        safeParseInt(game3_upper_4.value) +
+        safeParseInt(game3_upper_5.value) +
+        safeParseInt(game3_upper_6.value) > 62) {
+        _game3_upper_bonus = 35
+    } else {
+        _game3_upper_bonus = 0
+    }
+
+    _game3_lower_sum =
+        safeParseInt(game3_lower_3.value) +
+        safeParseInt(game3_lower_4.value) +
+        (game3_lower_fullHouse.checked === true ? 25 : 0) +
+        (game3_lower_smallStreet.checked === true ? 30 : 0) +
+        (game3_lower_bigStreet.checked === true ? 40 : 0) +
+        (game3_lower_kniffel.checked === true ? 50 : 0) +
+        safeParseInt(game3_lower_chance.value)
+
+    _game3_total_sum = _game3_upper_sum + _game3_upper_bonus + _game3_lower_sum
+
+    game3_upper_sum.innerText = _game3_upper_sum
+    game3_upper_bonus.innerText = _game3_upper_bonus
+    game3_lower_sum.innerText = _game3_lower_sum
+    game3_total_sum.innerText = _game3_total_sum
+
+    _game4_upper_sum =
+        safeParseInt(game4_upper_1.value) +
+        safeParseInt(game4_upper_2.value) +
+        safeParseInt(game4_upper_3.value) +
+        safeParseInt(game4_upper_4.value) +
+        safeParseInt(game4_upper_5.value) +
+        safeParseInt(game4_upper_6.value)
+
+    if (safeParseInt(game4_upper_1.value) +
+        safeParseInt(game4_upper_2.value) +
+        safeParseInt(game4_upper_3.value) +
+        safeParseInt(game4_upper_4.value) +
+        safeParseInt(game4_upper_5.value) +
+        safeParseInt(game4_upper_6.value) > 62) {
+        _game4_upper_bonus = 35
+    } else {
+        _game4_upper_bonus = 0
+    }
+
+    _game4_lower_sum =
+        safeParseInt(game4_lower_3.value) +
+        safeParseInt(game4_lower_4.value) +
+        (game4_lower_fullHouse.checked === true ? 25 : 0) +
+        (game4_lower_smallStreet.checked === true ? 30 : 0) +
+        (game4_lower_bigStreet.checked === true ? 40 : 0) +
+        (game4_lower_kniffel.checked === true ? 50 : 0) +
+        safeParseInt(game4_lower_chance.value)
+
+    _game4_total_sum = _game4_upper_sum + _game4_upper_bonus + _game4_lower_sum
+
+    game4_upper_sum.innerText = _game4_upper_sum
+    game4_upper_bonus.innerText = _game4_upper_bonus
+    game4_lower_sum.innerText = _game4_lower_sum
+    game4_total_sum.innerText = _game4_total_sum
+
+    _game5_upper_sum =
+        safeParseInt(game5_upper_1.value) +
+        safeParseInt(game5_upper_2.value) +
+        safeParseInt(game5_upper_3.value) +
+        safeParseInt(game5_upper_4.value) +
+        safeParseInt(game5_upper_5.value) +
+        safeParseInt(game5_upper_6.value)
+
+    if (safeParseInt(game5_upper_1.value) +
+        safeParseInt(game5_upper_2.value) +
+        safeParseInt(game5_upper_3.value) +
+        safeParseInt(game5_upper_4.value) +
+        safeParseInt(game5_upper_5.value) +
+        safeParseInt(game5_upper_6.value) > 62) {
+        _game5_upper_bonus = 35
+    } else {
+        _game5_upper_bonus = 0
+    }
+
+    _game5_lower_sum =
+        safeParseInt(game5_lower_3.value) +
+        safeParseInt(game5_lower_4.value) +
+        (game5_lower_fullHouse.checked === true ? 25 : 0) +
+        (game5_lower_smallStreet.checked === true ? 30 : 0) +
+        (game5_lower_bigStreet.checked === true ? 40 : 0) +
+        (game5_lower_kniffel.checked === true ? 50 : 0) +
+        safeParseInt(game5_lower_chance.value)
+
+    _game5_total_sum = _game5_upper_sum + _game5_upper_bonus + _game5_lower_sum
+
+    game5_upper_sum.innerText = _game5_upper_sum
+    game5_upper_bonus.innerText = _game5_upper_bonus
+    game5_lower_sum.innerText = _game5_lower_sum
+    game5_total_sum.innerText = _game5_total_sum
 }
